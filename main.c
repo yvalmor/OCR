de66975222f136352e4a9577bae6affd15db5f50
Yvon Morice <yvon.morice5@gmail.com>
2020-10-01 21:48:53 +0200

    Warning correction

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
     // Variables
-    GtkWidget * MainWindow = NULL;
+    GtkWidget *MainWindow = NULL;
 
     // GTK+ initialisation
     gtk_init(&argc, &argv);
@@ -12,9 +12,9 @@ int main(int argc, char **argv)
     // Main window creation
     MainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_signal_connect(G_OBJECT(MainWindow), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_window_set_title(GTK_WINDOW(MainWindow), "OCR");
+    gtk_window_set_title(MainWindow, "OCR");
     gtk_window_set_default_size(GTK_WINDOW(MainWindow), 1280, 720);
-    gtk_window_set_position(MainWindow, GTK_WIN_POS_CENTER);
+    gtk_window_set_position(GTK_WINDOW(MainWindow), GTK_WIN_POS_CENTER);
 
     // Main loop + showing main window
     gtk_widget_show(MainWindow);
