38d164ea3729d6e499a918a27127e58670b38b09
Yvon Morice <yvon.morice5@gmail.com>
2020-10-01 21:03:03 +0200

    base interface

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
 
 int main(int argc, char **argv)
 {
-    printf("hello\n");
-    return 0;
+    // Variables
+    GtkWidget * MainWindow = NULL;
+
+    // GTK+ initialisation
+    gtk_init(&argc, &argv);
+
+    // Main window creation
+    MainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(G_OBJECT(MainWindow), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+
+    // Main loop
+    gtk_widget_show(MainWindow);
+    gtk_main();
+
+    return EXIT_SUCCESS;
 }
