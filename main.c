b4e825b23fc95f2a63dd7752f5e138adb0c5649e
Yvon Morice <yvon.morice5@gmail.com>
2020-10-01 21:42:48 +0200

    Interface Modification (default size, position and title)

--- main.c
+++ main.c
@@ -12,9 +12,9 @@ int main(int argc, char **argv)
     // Main window creation
     MainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_signal_connect(G_OBJECT(MainWindow), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
-    void gtk_window_set_title(GTK_WINDOW(MainWindow), "OCR");
-    void gtk_window_set_default_size(GTK_WINDOW(MainWindow), 1280, 720);
-    void gtk_window_set_position(MainWindow, GTK_WIN_POS_CENTER);
+    gtk_window_set_title(GTK_WINDOW(MainWindow), "OCR");
+    gtk_window_set_default_size(GTK_WINDOW(MainWindow), 1280, 720);
+    gtk_window_set_position(MainWindow, GTK_WIN_POS_CENTER);
 
     // Main loop + showing main window
     gtk_widget_show(MainWindow);
