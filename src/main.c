97f47fb8adf8676ad51b7b76359e24eac68548f6
Yvon Morice <yvon.morice5@gmail.com>
2020-10-02 20:07:50 +0200

    makefile update

--- src/main.c
+++ src/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
+// Image
+static GtkWidget *Image;
+// Text view;
+static GtkWidget *TextView;
+
 // Prototypes
 void Analyse(GtkWidget *button, GtkWidget *file_selection);
 void Choose_image();
 void Set_image(GtkWidget *button, GtkWidget *file_selection)
 void Set_text(const gchar *path);
 int Check_file(const gchar *path, FILE *file);
-
-// Image
-static GtkWidget *Image;
-// Text view;
-static GtkWidget *TextView;
 
 int main(int argc, char **argv)
 {
