f32f4c35406f64c1b733f2cc79a2fc8037b53531
Yvon Morice <yvon.morice5@gmail.com>
2020-10-03 12:44:50 +0200

    Restructuration

---
+++ hdr/UI.h
@@ -0,0 +1,6 @@
+#ifndef OCR_UI_H
+#define OCR_UI_H
+
+void Setup(void);
+
+#endif //OCR_UI_H
---
+++ src/UI.c
@@ -0,0 +1,253 @@
+#include <gtk/gtk.h>
+
+#include "../hdr/UI.h"
+
+// Prototypes
+static void Window_setup(void);
+static void Image_setup(void);
+static void TextView_setup(void)
+
+static void Separator_setup(GtkWidget *vSep, GktWidget *hSep);
+static void Button_setup(GtkWidget *choose_button, GtkWidget *analyse_image);
+static void Container_setup(GtkWidget *vSep, GtkWidget *hSep,
+                     GktWidget *btn, GktWidget *img_btn);
+
+static void Analyse(GtkWidget *file_selection);
+static void Choose_image();
+static void Set_image(GtkWidget *file_selection);
+static void Set_text(const gchar *path);
+static int Check_file(const gchar *path, FILE *file);
+
+// Image
+static GtkWidget *Image;
+// Text view;
+static GtkWidget *TextView;
+// Windows
+static GtkWidget *Main_window;
+
+void Setup(void)
+{
+    // Variables
+    GtkWidget *vSeparator;
+    GtkWidget *hSeparator;
+
+    GtkWidget *choose_button;
+    GtkWidget *analyse_image;
+
+    Window_setup();
+    Image_setup();
+    TextView_setup();
+
+    Separator_setup(vSeparator, hSeparator);
+    Button_setup(choose_button, analyse_image);
+
+    Container_setup(vSeparator, hSeparator, choose_button, analyse_image);
+
+    gtk_widget_show_all(Main_window);
+}
+
+static void Window_setup(void)
+{
+    GObject window_obj = Main_window;
+    GtkWindow window = Main_window;
+
+    // Main window creation
+    Main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(window_obj, "delete-event", gtk_main_quit, NULL);
+
+    // Main window settings
+    gtk_window_set_title(window, "OCR");
+    gtk_window_set_default_size(window, 1280, 720);
+    gtk_window_set_position(window, GTK_WIN_POS_CENTER);
+}
+
+static void Image_setup(void)
+{
+    // Image initialisation
+    Image = gtk_image_new();
+}
+
+static void TextView_setup(void)
+{
+    // Text view initialisation
+    TextView = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(TextView), FALSE);
+}
+
+static void Separator_setup(GtkWidget *vSep, GtkWidget *hSep)
+{
+    // Separator initialisation
+    vSeparator = gtk_vseparator_new();
+    hSeparator = gtk_hseparator_new();
+}
+
+static void Button_setup(GtkWidget *choose_button, GtkWidget *analyse_image)
+{
+    GObject choose_obj = choose_button;
+    //GObject analyse_obj = analyse_image;
+
+    // Event binding
+    choose_button = gtk_button_new_with_label("Choose an image");
+    g_signal_connect_swapped(choose_obj,
+                             "clicked", Choose_image, NULL);
+
+    analyse_image = gtk_button_new_with_label("Analyse");
+    /*g_signal_connect_swapped(analyse_obj,
+                     "clicked", Analyse, path);*/
+}
+
+static void Container_setup(GtkWidget *vSep, GtkWidget *hSep,
+                     GktWidget *btn, GktWidget *img_btn)
+{
+    GtkWidget *scroll_bar;
+    GtkWidget *main_hBox;
+    GtkWidget *image_vBox;
+    GtkWidget *image_button_hBox;
+
+    GtkBox hBox = main_hBox;
+    GtkBox vBox = image_vBox;
+    GtkBox button_hBox = image_button_hBox;
+    GtkContainer scroll_container = scroll_bar;
+    GtkContainer window = Main_window;
+
+    // Containers initialisation
+    main_hBox = gtk_hbox_new(FALSE, 0);
+    image_vBox = gtk_vbox_new(FALSE, 0);
+    image_button_hBox = gtk_hbox_new(TRUE, 0);
+    scroll_bar = gtk_scrolled_window_new(NULL, NULL);
+
+    // Containers binding
+    gtk_box_pack_start(hBox, image_vBox, TRUE, TRUE, 0);
+    gtk_box_pack_start(hBox, vSep, FALSE, FALSE, 7);
+    gtk_box_pack_start(hBox, scroll_bar, TRUE, TRUE, 0);
+
+    gtk_box_pack_start(vBox, Image, TRUE, TRUE, 0);
+    gtk_box_pack_start(vBox, hSep, FALSE, FALSE, 7);
+    gtk_box_pack_start(vBox, image_button_hBox, FALSE, TRUE, 0);
+
+    gtk_box_pack_start(button_hBox, btn, TRUE, FALSE, 0);
+    gtk_box_pack_start(button_hBox, img_btn, TRUE, FALSE, 0);
+
+    gtk_container_add(scroll_container, TextView);
+    gtk_container_add(window, main_hBox);
+}
+
+//TODO
+static void Analyse(GtkWidget *file_selection)
+{
+    // Variables
+    const gchar *path;
+
+    path = gtk_file_selection_get_filename(GTK_FILE_SELECTION(file_selection));
+
+    /* pre-treatment
+     * treatment
+     * analysis
+     * display */
+
+    // Set_text("generated.txt");
+
+    printf("%s", path);
+}
+
+static void Choose_image()
+{
+    GtkWindow window = Main_window;
+    GtkWidget *file_chooser;
+    GtkFileChooserAction action;
+    GtkFileFilter *filter;
+    gint res;
+
+    action = GTK_FILE_CHOOSER_ACTION_OPEN;
+    filter = gtk_file_filter_new();
+    gtk_file_filter_add_pixbuf_formats(filter);
+
+    file_chooser = gtk_file_chooser_dialog_new (
+            "Open File",
+            window,
+            action,
+            _("_Cancel"),
+            GTK_RESPONSE_CANCEL,
+            _("_Open"),
+            GTK_RESPONSE_ACCEPT,
+            NULL);
+
+    gtk_file_chooser_dialog_add_filter(GTK_FILE_CHOOSER(file_chooser), filter);
+
+    res = gtk_dialog_run(GTK_DIALOG(file_chooser));
+    if (res == GTK_RESPONSE_ACCEPT)
+        Set_image(file_chooser);
+    else
+        gtk_widget_destroy(file_chooser);
+}
+
+static void Set_image(GtkWidget *file_chooser)
+{
+    // Variables
+    const gchar *path;
+
+    // Getting the path of the image and setting it
+    path = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(file_chooser));
+    gtk_image_set_from_file(GTK_IMAGE(Image), path);
+
+    gtk_widget_destroy(file_chooser);
+}
+
+static void Set_text(const gchar *path)
+{
+    // Variables
+    GtkTextBuffer *buffer;
+    GtkTextIter start;
+    GtkTextIter end;
+    FILE *file;
+    gchar read[1024];
+
+    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(TextView));
+
+    // File opening
+    file = fopen(path, "rt");
+
+    // Check if file is NULL
+    if (!Check_file(path, file))
+        return;
+
+    // File reading
+    gtk_text_buffer_get_start_iter(buffer, &start);
+    gtk_text_buffer_get_end_iter(buffer, &end);
+    gtk_text_buffer_delete(buffer, &start, &end);
+
+    while (fgets(read, 1024, file))
+    {
+        gtk_text_buffer_get_end_iter(buffer, &end);
+        gtk_text_buffer_insert(
+                buffer, &end,
+                g_locale_to_utf8(read, -1, NULL, NULL, NULL), -1);
+    }
+
+    // File closing
+    fclose(file);
+}
+
+static int Check_file(const gchar *path, FILE *file)
+{
+    if (file == NULL)
+    {
+        GtkWidget *dialog;
+
+        // Opening of a dialog to signal the user a problem
+        dialog = gtk_message_dialog_new(
+                GTK_WINDOW(Main_window),
+                GTK_DIALOG_MODAL,
+                GTK_MESSAGE_ERROR,
+                GTK_BUTTONS_OK,
+                "Impossible to open file:\n%s",
+                g_locale_to_utf8(path, -1, NULL, NULL, NULL));
+
+        gtk_dialog_run(GTK_DIALOG(dialog));
+        gtk_widget_destroy(dialog);
+
+        return 0;
+    }
+
+    return 1;
+}
