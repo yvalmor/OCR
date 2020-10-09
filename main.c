b5d0e5d5636f03cf7b8fe038592b6cd5e145a1e3
Yvon Morice <yvon.morice5@gmail.com>
2020-10-09 22:14:16 +0200

    tests on bitmap images

--- main.c
+++ main.c
@@ -18,9 +18,9 @@ int main()
 
     char bitmap[] =
             {
-                0xff,
-                0xff,
-                0xff,
+                0x00,
+                0x00,
+                0x00,
                 0x00
             };
 
