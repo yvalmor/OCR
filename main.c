e8ec3cd10732cc9b10163eb86b7b8320c7306255
Yvon Morice <yvon.morice5@gmail.com>
2020-10-09 22:10:14 +0200

    tests on bitmap images

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
-
-#include "bitmap.h"
+#include <stdlib.h>
 
 int main()
 {
-    BITMAPINFOHEADER bitmapInfoHeader;
-    unsigned char *bitmapData;
+    int width = 1;
+    int height = 1;
 
-    bitmapData = LoadBitmapFile("test.bmp", &bitmapInfoHeader);
+    char tag[] = {'B', 'M'};
 
-    printf("%s", bitmapData);
+    int header[] =
+            {
+                0x3a, 0x00, 0x36, 0x28,
+                width, height,
+                0x010001, 0, 0,
+                0x002e23, 0x002e23, 0, 0
+            };
+
+    char bitmap[] =
+            {
+                0x01,
+                0x00
+            };
+
+    FILE *fp = fopen("test.bmp", "w+");
+    fwrite(&tag, sizeof(tag), 1, fp);
+    fwrite(&header, sizeof(header), 1, fp);
+    fwrite(&bitmap, sizeof(bitmap), 1, fp);
+    fclose(fp);
 
     return 0;
 }
