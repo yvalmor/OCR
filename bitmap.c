b78816ed659df6804024b3b80a4691ed411b745c
Yvon Morice <yvon.morice5@gmail.com>
2020-10-09 21:44:11 +0200

    tests on bitmap images

---
+++ bitmap.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "bitmap.h"
+
+unsigned char *LoadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader)
+{
+    FILE *filePtr;
+    BITMAPFILEHEADER bitmapFileHeader;
+    unsigned char *bitmapImage;
+    int imageIdx = 0;
+    unsigned char tempRGB;
+
+    filePtr = fopen(filename, "rb");
+    if (filePtr == NULL)
+        return NULL;
+
+    fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, filePtr);
+
+    if (bitmapFileHeader.bfType != 0x4D42)
+    {
+        fclose(filePtr);
+        return NULL;
+    }
+
+    fread(bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, filePtr);
+
+    fseek(filePtr, bitmapFileHeader.bfOffBits, SEEK_SET);
+
+    bitmapImage = malloc(bitmapInfoHeader -> biSizeImage);
+
+    if (!bitmapImage)
+    {
+        free(bitmapImage);
+        fclose(filePtr);
+        return NULL;
+    }
+
+    fread(bitmapImage, bitmapInfoHeader -> biSizeImage, filePtr);
+
+    if (bitmapImage == NULL)
+    {
+        fclose(filePtr);
+        return NULL;
+    }
+
+    for (imageIdx = 0; imageIdx < bitmapInfoHeader -> biSizeImage; imageIdx += 3)
+    {
+        tempRGB = bitmapImage[imageIdx];
+        bitmapImage[imageIdx] = bitmapImage[imageIdx + 2];
+        bitmapImage[imageIdx + 2] = tempRGB;
+    }
+
+    fclose(filePtr);
+    return bitmapImage;
+}
---
+++ bitmap.h
@@ -0,0 +1,31 @@
+#ifndef OCR_BITMAP_H
+#define OCR_BITMAP_H
+
+typedef struct tabBITMAPFILEHEADER
+{
+    WORD bfType;
+    DWORD bfSize;
+    WORD bfReserved1;
+    WORD bfReserved2;
+    DWORD bfOffBits;
+}BITMAPFILEHEADER;
+
+typedef struct tabBITMAPINFOHEADER
+{
+    DWORD biSize;  //specifies the number of bytes required by the struct
+    LONG biWidth;  //specifies width in pixels
+    LONG biHeight;  //species height in pixels
+    WORD biPlanes; //specifies the number of color planes, must be 1
+    WORD biBitCount; //specifies the number of bit per pixel
+    DWORD biCompression;//spcifies the type of compression
+    DWORD biSizeImage;  //size of image in bytes
+    LONG biXPelsPerMeter;  //number of pixels per meter in x axis
+    LONG biYPelsPerMeter;  //number of pixels per meter in y axis
+    DWORD biClrUsed;  //number of colors used by th ebitmap
+    DWORD biClrImportant;  //number of colors that are important
+}BITMAPINFOHEADER;
+
+unsigned char *LoadBitmapFile(char *filename,
+                              BITMAPINFOHEADER *bitmapInfoHeader)
+
+#endif //OCR_BITMAP_H
