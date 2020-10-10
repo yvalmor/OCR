04004578d220b37b8af9149285b0474fbfafe9d6
Yvon Morice <yvon.morice5@gmail.com>
2020-10-10 13:54:12 +0200

    Tries on line detection

--- src/main.c
+++ src/main.c
@@ -38,8 +38,6 @@ int main()
         for (int j = 0; j < height; ++j)
             pixels[i][j] = placeholder[i][j];
 
-    free(placeholder);
-
     int clusters[width][height];
 
     initClusters(clusters, width, height);
