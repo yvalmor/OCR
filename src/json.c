7eb65af9e13622c68f4c81368eb59c58fb70f81a
Souleymane Sentici <souleymanesentici@live.com>
2020-12-04 17:16:35 +0100

    changes to writer

--- src/json.c
+++ src/json.c
@@ -1,47 +1,55 @@
 #include <stdio.h>
-#include "network.h"
+#include "../hdr/network.h"
 #include <json-c/json.h>
 
-void write_neuron(Neuron *neuron, int index, FILE *fp)
+void write_neuron(Neuron *neuron, int index, int index_max, FILE *fp)
 {
     int n = neuron->len_weight;
 
     fprintf(fp, " \"neuron%d\" : {\n\"len_weight\" : %d,\n\"biais\" : %d,\n\"value\" : %f,\n\"activated\" : %f,\n\"weights\" : [\n", 
             index, n, neuron->biais, neuron->value, neuron->activated);
 
-    for(int i = 0; i < n; i++)
-        fprintf(fp, " %f,\n", weights[i]);
+    for(int i = 0; i < n-1; i++)
+        fprintf(fp, " %f,\n", neuron->weights[i]);
 
-    fprintf(fp, " ]\n}");
+    fprintf(fp, " %f\n", neuron->weights[n-1]);
+
+    if (index != index_max)
+        fprintf(fp, " ]\n}, ");
+    else
+        fprintf(fp, " ]\n]} ");
 }
 
-void write_layer(Layer *layer, int index, FILE *fp)
+void write_layer(Layer *layer, int index, int index_max, FILE *fp)
 {
     int n = layer->len_neurons;
 
     fprintf(fp, " \"layer%d\" : {\n\"len_neurons\" : %d,\n\"neurons\" : [\n", index, n);
 
-    for(int i = 0; i < n; i++)
-        write_neuron(layer->neurons[i], i, fp);
-
-    fprintf(fp, " ]\n}");
+    for(int i = 0; i < n-1; i++)
+        write_neuron(&layer->neurons[i], i, n-1, fp);
+    
+    if (index != index_max)
+        fprintf(fp, " ]\n}, ");
+    else
+        fprintf(fp, " ]\n} ");
 }
 
 void write_network(Network *network, char *filename)
 {
-    int n = newtork->nbLayers;
+    int n = network->nbLayers;
     FILE *fp = fopen(filename, "a");
 
     if(!fp)
     {
-        fprintf(stderr, "write_network() error: file open failed '%s'.\n", fname);
+        fprintf(stderr, "write_network() error: file open failed '%s'.\n", filename);
         return;
     }
 
     fprintf(fp, "{\"network\" : {\n\"nbLayers\" : %d,\n\"layers\" : [\n", n);
 
     for(int i = 0; i < n; i++)
-        write_layer(network->layers[i], i, fp);
+        write_layer(&network->layers[i], i, n-1, fp);
 
     fprintf(fp, " ]\n}");
     fclose(fp);
@@ -73,7 +81,8 @@ Network *parse_network(char *filename)
 int main()
 {
    Network *network = malloc(sizeof(network));
-   create_network(network, 3, 2, 2);
+   network->nbLayers = 4;
+   create_network(network, 4, 2, 2);
 
    write_network(network, "network.json");
 }
